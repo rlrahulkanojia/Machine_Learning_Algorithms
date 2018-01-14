6bbf5ca29ea6384e0d6e7b0e1765c5d4981d4553
unknown <rlrahulkanojia@gmail.com>
2018-01-14 04:02:05 -0800

    Algorithm

---
+++ 16 Apriori_Python/apriori.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 25 20:04:21 2017
+
+@author: Abstergo
+"""
+# Apriori
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Data Preprocessing
+dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
+transactions = []
+for i in range(0, 7501):
+    transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
+
+# Training Apriori on the dataset
+from apyori import apriori
+rules = apriori(transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
+
+# Visualising the results
+results = list(rules)
